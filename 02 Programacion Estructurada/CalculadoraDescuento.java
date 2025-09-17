bf427a8ac379edf4c90d821ea6cc81dfe9e75ea6
Alex D <a_p_dauria@hotmail.com>
2025-09-17 12:48:09 +0200

    PDF agregado

--- 02 Programacion Estructurada/CalculadoraDescuento.java
+++ 02 Programacion Estructurada/CalculadoraDescuento.java
@@ -2,8 +2,6 @@
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class CalculadoraDescuento {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
